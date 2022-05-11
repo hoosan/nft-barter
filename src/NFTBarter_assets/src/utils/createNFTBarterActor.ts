21a86378ca27f925d390f904893a4436f0e6084d
hoosan <hoosanalis@gmail.com>
2022-05-11 23:07:24 +0900

    Modify

--- src/NFTBarter_assets/src/utils/createNFTBarterActor.ts
+++ src/NFTBarter_assets/src/utils/createNFTBarterActor.ts
@@ -1,16 +1,11 @@
-import { IDL } from '@dfinity/candid';
+// @ts-ignore
+import { idlFactory } from '../../declarations/NFTBarter/NFTBarter.did.js';
 
-declare module '../../../declarations/NFTBarter/NFTBarter.did.js' {
-  function idlFactory(): IDL.ServiceClass;
-}
-import {
-  NFTBarter,
-  idlFactory,
-} from '../../../declarations/NFTBarter/NFTBarter.did.js';
+import { _SERVICE as INFTBarter } from '../../../declarations/NFTBarter/NFTBarter.did.js';
 import { curriedCreateActor } from '../../../NFTBarter_assets/src/utils/createActor';
 import localCanisterIds from '../../../../.dfx/local/canister_ids.json';
 
 const canisterId = localCanisterIds.NFTBarter.local;
 
 export const createNFTBarterActor =
-  curriedCreateActor<NFTBarter>(idlFactory)(canisterId);
+  curriedCreateActor<INFTBarter>(idlFactory)(canisterId);
