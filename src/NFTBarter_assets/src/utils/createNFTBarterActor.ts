03133e052f7dbc7a821b4ad8adaeb5b69b538647
hoosan <hoosanalis@gmail.com>
2022-05-11 21:04:40 +0900

    Add e2e tests for user registration

---
+++ src/NFTBarter_assets/src/utils/createNFTBarterActor.ts
@@ -0,0 +1,15 @@
+import { IDL } from '@dfinity/candid';
+
+declare module '../../../declarations/NFTBarter/NFTBarter.did.js' {
+  function idlFactory(): IDL.ServiceClass;
+}
+import {
+  NFTBarter,
+  idlFactory,
+} from '../../../declarations/NFTBarter/NFTBarter.did.js';
+import { curriedCreateActor } from '../../../NFTBarter_assets/src/utils/createActor';
+import localCanisterIds from '../../../../.dfx/local/canister_ids.json';
+
+const canisterId = localCanisterIds.NFTBarter.local;
+export const createNFTBarterActor =
+  curriedCreateActor<NFTBarter>(idlFactory)(canisterId);
---
+++ src/tests/e2e/registration.test.ts
@@ -0,0 +1,34 @@
+import { jest } from '@jest/globals';
+import { Secp256k1KeyIdentity } from '@dfinity/identity';
+import fetch from 'isomorphic-fetch';
+import { createNFTBarterActor } from '../../NFTBarter_assets/src/utils/createNftBarterActor';
+
+const identityOptionOfAlice = {
+  agentOptions: {
+    identity: Secp256k1KeyIdentity.generate(),
+    fetch,
+    host: 'http://localhost:8000',
+  },
+};
+const actorOfAlice = createNFTBarterActor(identityOptionOfAlice);
+
+jest.setTimeout(60000);
+
+describe('User registration tests', () => {
+  it('Alice is not registered yet.', async () => {
+    expect(await actorOfAlice.isRegistered()).toBe(false);
+  });
+
+  it('Alice can be registered.', async () => {
+    const res = await actorOfAlice.register();
+    if ('ok' in res) {
+      expect(res.ok._isPrincipal).toBe(true);
+    } else {
+      throw new Error(res.err);
+    }
+  });
+
+  it('Alice is already registered.', async () => {
+    expect(await actorOfAlice.isRegistered()).toBe(true);
+  });
+});
